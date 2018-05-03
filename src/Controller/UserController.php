d4f7c37c18c9c45e0575412ba4516ab6917ed18c
BartekLewy <lewandowskibartosz@icloud.com>
2018-05-03 22:25:22 +0200

    add new controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+
+class UserController extends Controller
+{
+    public function index(): Response
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        return $this->render('user/index.html.twig', ['users' => $users]);
+    }
+}
